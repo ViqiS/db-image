ae41a93531e7630efabc773c219f41d968b583c6
Victoria Escalada <viqiescalada@gmail.com>
2023-08-07 16:16:39 -0300

    agrego errorHandler

--- api/index.js
+++ api/index.js
@@ -2,13 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const routerApi = require('./routes');
 const cors = require('cors');
+const errorHandler = require('./middlewares/errorHandler')
 
 const app = express();
 const port = process.env.PORT ?? 8080;
 
 app.use(express.json());
 
-const whiteList = ['http://localhost:3000', 'https://db-image-dev.fl0.io/'];
+const whiteList = ['http://localhost:3000', 'https://db-image-dev.fl0.io'];
 const options = {
   origin: (origin, callback) => {
     if (whiteList.includes(origin) || !origin) {
@@ -25,9 +26,14 @@ app.get('/', (req, res) => {
   res.send('hola!');
 });
 
+routerApi(app);
+
+// Agrega el middleware errorHandler después de todas las rutas y middlewares
+app.use(errorHandler);
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
 
 
-routerApi(app);
+
