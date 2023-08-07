df3d2012c6e882546a8a67518cae61dfc01ae1fe
Victoria Escalada <viqiescalada@gmail.com>
2023-08-07 14:57:08 -0300

    Cambios de config

--- api/libs/sequelize.js
+++ api/libs/sequelize.js
@@ -3,17 +3,17 @@ const { Sequelize } = require('sequelize');
 const setupModels = require('../db/models');
 
 const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config.json')[env];
+const config = require('../config/config.js')[env];
 
 let sequelize;
 
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, {
-    host: config.dbHost,
-    port: config.dbPort,
-    dialect: 'postgres',
+    host: config.host,
+    port: config.port,
+    dialect: config.dialect,
     logging: false,
     dialectOptions: {
       ssl: {
@@ -23,6 +23,7 @@ if (config.use_env_variable) {
     },
   });
 }
+
 
 setupModels(sequelize);
 
